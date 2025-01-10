7a4e54b6c5e878c2e709cf8815af0628b2240b63
Tiago Porto <tiago@tiagoporto.com>
2025-01-10 20:18:58 -0300

    Update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,5 +5,5 @@ import globals from 'globals'
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { languageOptions: { globals: globals.node } },
-  ...tpConfig.configs.default,
+  ...tpConfig.configs.base,
 ]
